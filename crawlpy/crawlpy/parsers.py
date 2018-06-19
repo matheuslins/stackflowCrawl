aab186a01b57b772c4bde9145a8e019cd8c6e520
Matheus Lins <msl@cin.ufpe.br>
2018-06-19 03:51:16 -0300

    create parsers

---
+++ crawlpy/crawlpy/parsers.py
@@ -0,0 +1,9 @@
+import re
+
+_re_nonspaces = re.compile(u'[^\s]+|\n', flags=re.UNICODE)
+_re_norm_CR = re.compile(u'(?:\s*\n\s*)+', flags=re.UNICODE)
+
+
+def normalize_spaces(value):
+    cleared_value = u' '.join(_re_nonspaces.findall(value))
+    return _re_norm_CR.sub(u'\n', cleared_value).strip()
