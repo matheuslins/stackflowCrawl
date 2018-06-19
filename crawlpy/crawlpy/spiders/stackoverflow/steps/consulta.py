d241bd98f20a7b8b143bc2a72ddddab60fbcd297
Matheus Lins <msl@cin.ufpe.br>
2018-06-19 03:55:09 -0300

    consult and extract jobs

--- crawlpy/crawlpy/spiders/stackoverflow/steps/consulta.py
+++ crawlpy/crawlpy/spiders/stackoverflow/steps/consulta.py
@@ -1,2 +1,14 @@
 # -*- coding: utf-8 -*-
+from scrapy.http import Request
+from urllib.parse import urljoin
 
+from .extracao import extract_job
+from ..constants.consulta import XPAHS_CONSULT
+from ..utils import clean_url
+
+
+def consult_job(spider, response):
+    jobs_urls = response.xpath(XPAHS_CONSULT['jobs_urls'])
+    for job_url in jobs_urls:
+        final_url = clean_url(urljoin(response.url, job_url.extract()))
+        yield Request(final_url, callback=extract_job)
--- crawlpy/crawlpy/spiders/stackoverflow/steps/extracao.py
+++ crawlpy/crawlpy/spiders/stackoverflow/steps/extracao.py
@@ -1,2 +1,19 @@
 # -*- coding: utf-8 -*-
 
+from crawlpy.loaders import JobLoader
+
+from ..utils import extract_job_id
+from ..constants.extracao import XPATHS_JOB
+
+
+def extract_job(response):
+    loader = JobLoader(response=response)
+    loader.add_xpaths(XPATHS_JOB)
+    loader.add_value('job_id', extract_job_id(response.url))
+    loader.add_value('job_url', response.url)
+    item = loader.load_item()
+    return item
+
+
+def extract_company(spider, response):
+    pass
