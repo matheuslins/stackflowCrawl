588cdf01ab33efcc5258eb14db4b50d79b6363de
Matheus Lins <msl@cin.ufpe.br>
2018-06-19 03:55:33 -0300

    create utils file of stackoverflow spider

--- crawlpy/crawlpy/spiders/stackoverflow/utils.py
+++ crawlpy/crawlpy/spiders/stackoverflow/utils.py
@@ -1,2 +1,14 @@
 # -*- coding: utf-8 -*-
+import re
 
+
+def extract_job_id(url):
+    job_id = re.findall(r'(?<=https:\/\/stackoverflow\.com\/jobs\/)(\d+)', url)
+    return job_id[0] if job_id else ''
+
+
+def clean_url(url):
+    cleaned_url = re.findall(
+        "https:\/\/stackoverflow\.com\/jobs\/\d+\/.*", url)
+    if cleaned_url:
+        return cleaned_url[0]
