b4f3532ebe1a62c39c28aba617a14fa1972b033d
Matheus Lins <msl@cin.ufpe.br>
2018-06-19 03:50:11 -0300

    create item structure

--- crawlpy/crawlpy/items.py
+++ crawlpy/crawlpy/items.py
@@ -6,9 +6,43 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import Identity
 
 
-class CrawlpyItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class JobItem(scrapy.Item):
+    # General infos
+    job_url = scrapy.Field()
+    job_id = scrapy.Field()
+    job_title = scrapy.Field()
+    company = scrapy.Field()
+    location = scrapy.Field()
+    sponsor = scrapy.Field()
+    salary_year = scrapy.Field()
+    paid = scrapy.Field()
+
+    # About the job
+    job_type = scrapy.Field()
+    industry = scrapy.Field()
+    experience_level = scrapy.Field()
+    role = scrapy.Field()
+    company_size = scrapy.Field()
+    company_type = scrapy.Field()
+    job_description = scrapy.Field()
+    link_apply = scrapy.Field()
+    joel_test = scrapy.Field(output_processor=Identity())  # list
+
+    # Tecnologies
+    tecnologies = scrapy.Field(output_processor=Identity())  # list
+
+    # Benefits
+    benefits = scrapy.Field(output_processor=Identity())  # list
+
+
+class CompanyItem(scrapy.Item):
+    # General infos
+    name = scrapy.Field()
+    company_type = scrapy.Field()
+    company_home_page = scrapy.Field()
+    open_jobs = scrapy.Field(output_processor=Identity())  # list
+    benefits = scrapy.Field(output_processor=Identity())  # list
+    technology_stack = scrapy.Field(output_processor=Identity())  # list
