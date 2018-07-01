d140535c137d0b586fc1b6a14c942ad0b16beaf1
Matheus Lins <msl@cin.ufpe.br>
2018-06-30 21:03:41 -0300

    create a func helper to clean skill field

--- crawlpy/crawlpy/spiders/stackoverflow/constants/extracao.py
+++ crawlpy/crawlpy/spiders/stackoverflow/constants/extracao.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 
 from scrapy.loader.processors import Join, TakeFirst
 from crawlpy.processors import TakeLast
@@ -7,6 +8,24 @@ from crawlpy.processors import TakeLast
 _base_xpath_job = ('//div[contains(@class, "job-details--about")]/'
                    '/div[@class="mb8"]//span[preceding-sibling::span['
                    'contains(., "{}")]]/text()').format
+
+COMMON_SKILLS = ['docker', 'ansible', 'linux', 'python', 'django', 'c#',
+                'java', 'flask', 'c++', '.net', 'javascript', 'elixir',
+                'html', 'html5', 'css', 'css3', 'redux', 'react', 'angular',
+                'less', 'swift', 'objective-c', 'jquery', 'php', 'wordpress']
+
+def _clean_skills(skills):
+    pattern = re.compile(
+        r'(iphone|http|https|http:\/\/.*|https:\/\/.*|www|\.com|\.com\..*|@)')
+    new_list = []
+    for element in skills:
+        if not re.findall(pattern, element):
+            for skill in COMMON_SKILLS:
+                if skill in element:
+                    element = skill
+            new_list.append(element)
+    return list(set(new_list))
+
 
 XPATHS_JOB = {
 
@@ -18,7 +37,8 @@ XPATHS_JOB = {
     'industry': _base_xpath_job('Industry'),
     'company_size': _base_xpath_job('Company size'),
     'company_type': _base_xpath_job('Company type'),
-    'tecnologies': '//section[contains(., "Technologies")]//div//a/text()',
+    'tecnologies': ('//section[contains(., "Technologies")]//div//a/text()',
+                    _clean_skills),
 
     'job_description': (
         '//section[contains(., "Job description")]//p//text() | '
@@ -33,8 +53,9 @@ XPATHS_JOB = {
     'company': ('//h1[contains(@class, "headline1")]/'
                 'following-sibling::div[1]//a//text()'),
     'location': ('//h1[contains(@class, "headline1")]/'
-                 'following-sibling::div[1]//span/text()', TakeLast()),
-    'salary': ('//span[contains(@class, "-salary")]/text()', TakeFirst()),
+                 'following::span[contains(@class, "fc-black-500")]'
+                 '//span/following::text()[1]', TakeFirst()),
+    'salary': '//header//span[contains(@class, "-salary")]/text()',
     'sponsor': ('//span[contains(@class, "-visa")]/text()', TakeFirst()),
     'paid': ('//span[contains(@class, "-relocation")]/text()', TakeFirst())
 }
