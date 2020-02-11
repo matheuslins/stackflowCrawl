ffbdfe800c133c3e58a56a6f2da2256db6b1d429
Matheus Lins <matheuslins9@gmail.com>
2020-02-11 07:49:14 -0300

    Hash id

--- stackflowCrawl/settings.py
+++ stackflowCrawl/settings.py
@@ -5,8 +5,8 @@ from scrapy.utils.project import get_project_settings
 settings = get_project_settings()
 
 
-DEBUG = config('DEBUG', cast=bool, default=settings.get('DEBUG'))
-ENVIRONMENT = config('ENVIRONMENT', cast=str, default=settings.get('ENVIRONMENT'))
+DEBUG = config('DEBUG', cast=bool, default=False)
+ENVIRONMENT = config('ENVIRONMENT', cast=str, default='hml')
 
 BOT_NAME = 'stackflowCrawl'
 
@@ -95,11 +95,11 @@ ITEM_PIPELINES['stackflowCrawl.pipelines.ElasticSearchPipeline'] = 300
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 # Elastic Search
-ES_HOST = config('ES_HOST', cast=str, default=settings.get('ES_HOST'))
+ES_HOST = config('ES_HOST', cast=str, default='https://8a7b0b59fcde4d50ae94d5027ffbaba0.us-east-1.aws.found.io:9243')
 ES_INDEX = config('ES_INDEX', cast=str, default='stkflow-jobs')
 
-ES_CLUSTER_USER = config('ES_CLUSTER_USER', cast=str, default=settings.get('ES_CLUSTER_USER'))
-ES_CLUSTER_PASS = config('ES_CLUSTER_PASS', cast=str, default=settings.get('ES_CLUSTER_PASS'))
+ES_CLUSTER_USER = config('ES_CLUSTER_USER', cast=str, default='elastic')
+ES_CLUSTER_PASS = config('ES_CLUSTER_PASS', cast=str, default='wL7S8rUNIRh2bXKQ5lvy53Pr')
 
 RETRY_HTTP_CODES = [429]
 
