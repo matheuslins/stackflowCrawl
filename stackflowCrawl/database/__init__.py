dabe816418bfc8795196d8fee17c7f92ddabf2f8
Matheus Lins <matheuslins9@gmail.com>
2020-02-10 22:23:36 -0300

    ES CLUSTER config

--- stackflowCrawl/database/__init__.py
+++ stackflowCrawl/database/__init__.py
@@ -2,13 +2,19 @@ import certifi
 
 from elasticsearch import Elasticsearch
 
-from stackflowCrawl.settings import ES_HOST, ENVIRONMENT, ES_INDEX
+from stackflowCrawl.settings import ES_HOST, ENVIRONMENT, ES_INDEX, ES_CLUSTER_PASS, ES_CLUSTER_USER
 
 
 def config_client():
     if ENVIRONMENT == 'dev':
-        return Elasticsearch(hosts=ES_HOST, timeout=25)
-    return Elasticsearch(hosts=ES_HOST, use_ssl=True, ca_certs=certifi.where(), timeout=25)
+        return Elasticsearch(hosts=ES_HOST, timeout=25, http_auth=(ES_CLUSTER_USER, ES_CLUSTER_PASS))
+    return Elasticsearch(
+        hosts=ES_HOST,
+        use_ssl=True,
+        ca_certs=certifi.where(),
+        timeout=25,
+        http_auth=(ES_CLUSTER_USER, ES_CLUSTER_PASS)
+    )
 
 
 if __name__ == '__main__':
